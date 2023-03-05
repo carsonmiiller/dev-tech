e3400b3a62c01d5e0bec0514ff0ed374456c8726
Keegan Johnson <kejohnson32@wisc.edu>
2023-03-05 14:51:15 -0600

    Upload ThisJDBC.java

---
+++ ThisJDBC.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+class MysqlCon{
+    public static void main(String args[]){
+        try{
+            Connection dbCxn=DriverManager.getConnection(
+                "jdbc:mysql://localhost:43306/ThisAndThat", "root", "tatdat4"
+            );
+            Statement selectThis=dbCxn.createStatement();
+            ResultSet rsThis=selectThis.executeQuery("select * from This");
+            while(rsThis.next()){
+                System.out.println(rsThis.getInt(1)
+                            + "  " + rsThis.getString(2)
+                            + "  " + rsThis.getInt(3)
+                            + "  " + rsThis.getDate(4)
+                            + "  " + rsThis.getString(5)
+                );
+            }
+            dbCxn.close();
+        }catch(Exception e){ System.out.println(e);}
+    }
+}

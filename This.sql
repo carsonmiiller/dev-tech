47f581128378479bc33574d5aa8dd96153386de1
Keegan Johnson <keeganj@cs506-team-38.cs.wisc.edu>
2023-03-05 14:02:52 -0600

    This.sql file

---
+++ This.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `ThisAndThat`.`This` (
+  `This_pk` INT NOT NULL auto_increment,
+  `This_abc` VARCHAR(45) NULL,
+  `This_def` INT NULL,
+  `This_ghi` DATETIME NULL,
+  `This_jkl` ENUM('x', 'y', 'z') NULL,
+  PRIMARY KEY (`This_pk`));

42eb82d6d13f2b8398a28455848ae0dd9da022c8
Keegan Johnson <kejohnson2@mac.com>
2023-03-27 19:56:46 -0500

    added method implementation code

--- EntityThis.java
+++ EntityThis.java
@@ -16,6 +16,8 @@ public class EntityThis{
     private int This_def;
     private java.sql.Date This_ghi;
     private This_jkl This_jkl;
+
+    private static JDBCConnectionMaker dbCxnMaker;
     
 
     /**
@@ -25,7 +27,13 @@ public class EntityThis{
      * a new instance of the class to be inserted into the database.
      * @return an instance of the EntityThis class
      */
-    public EntityThis(){}
+    public EntityThis(){
+        // validate existense of dbcxnmaker
+
+        // creating an empty entityThis instance
+
+        // return EntityThis
+    }
 
     /**
      * This is a constructor for the EntityThis class.
@@ -35,7 +43,15 @@ public class EntityThis{
      * @param primaryKey
      * @return an instance of the EntityThis class
      */
-    public EntityThis(int primaryKey){}
+    public EntityThis(int primaryKey){
+        // validate existense of dbcxnmaker
+
+        // use primary key to access exisiing row in table 
+        
+        // use to set values of instance vars
+
+        //return EntityThis
+    }
 
     // Getters and Setters
     public String getThis_abc(){return This_abc;}
@@ -50,56 +66,84 @@ public class EntityThis{
     public This_jkl getThis_jkl(){return This_jkl;}
     public void setThis_jkl(This_jkl This_jkl){this.This_jkl = This_jkl;}
 
+    public void setDbCxnMaker(JDBCConnectionMaker dbCxnMaker){
+        this.dbCxnMaker = dbCxnMaker;
+    }
+
     /**
      * This method executes an INSERT INTO statement on the This table.
      * It creates a new record in the table.
      * It could be called by the save() method.
-     * @return:  true if creation was successful, false otherwise
      */
-    private boolean create(){}
-
+    private void create(){
+        // use JDBC Connection to initialize Statement object
+        
+        // execute INSERT INTO query on Statement object using instance variables
+    }
+    
     /**
      * This method executes a SELECT statement on the This table.
      * It selects all columns for a particular primary key value.
      * It is called by the load() method.
      * @param:  primaryKey - the primary key value of the record to be read
-     * @return:  true if read was successful, false otherwise
      */
-    private boolean read(int primaryKey){}
-
+    private void read(int primaryKey){
+        // use JDBC Connection to initialize Statement object
+        
+        // execute SELECT query on Statement object using primary key variable
+        
+        // set instance variables to results from query
+    }
+    
     /**
      * This method executes an UPDATE statement on the This table.
      * It updates the table with the values of the instance variables
      * in this EntityThis object.
      * It could be called by the save() method.
-     * @return:  true if update was successful, false otherwise
      */
-    private boolean update(){}
-
+    private void update(){
+        // use JDBC Connection to initialize Statement object
+        
+        // execute UPDATE query on Statement object using instance variables
+    }
+    
     /**
      * This method executes a DELETE statement on the This table.
      * It deletes the record for this instance of the EntityThis class.
      * It is called by the remove() method.
-     * @return:  true if deletion was successful, false otherwise
      */
-    private boolean delete(){}
+    private void delete(){
+        // use JDBC Connection to initialize Statement object
+        
+        // execute DELETE query on Statement object using instance's primary key
+    }
 
     /**
      * This method calls create() or update(), whichever is appropriate.
      * create() is called if the primaryKey value does not exist in the This table
      * update() is called if the primaryKey value exists in the This table
      */
-    public void save(){}
+    public void save(){
+        // validate existence of primary key in table
+
+        // if exists, call update()
+
+        // if does not exist, call create()
+    }
 
     /**
      * This method calls read() to "pick up" the record for a particular primary key value.
      * @param:  primaryKey - the primary key value of the record to be read
      */
-    public void load(int primaryKey){}
+    public void load(int primaryKey){
+        // call read() to pick up the record corresponding to primary key
+    }
 
     /**
      * This method calls delete().
      * It removes the record for this instance of the EntityThis class from the This table
      */
-    public void remove(){}
+    public void remove(){
+        // call delete() to remove record for this instance of the table
+    }
 }
